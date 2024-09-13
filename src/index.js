1e0046c4bf277cf34873dfab65cb166d28eabff3
Yui Fan <whisama613@gmail.com>
2024-09-13 17:10:20 +0700

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const PORT = 3000
 
 const app = express()
+Number = 20;
+Number = 23;
 
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`)
 })
+
