38b41c5ad1260ece63f988d78d60719b55397c8c
ManhCuong2003 <tranmanhcuong11302@gmail.com>
2024-10-04 16:30:08 +0700

    get product by id

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -4,3 +4,9 @@ exports.getAllProductsController = async (req, res) => {
   const response = await productServices.getAllProduct()
   return res.status(200).json(response)
 }
+
+exports.getProductById = async (req, res) => {
+  const { productId } = req.params
+  const response = await productServices.getProductById(productId)
+  return res.status(200).json(response)
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,10 +2,12 @@ const express = require('express')
 const { accessTokenValidator } = require('../middlewares/auth.middlewares')
 const wrapAsync = require('../utils/handler')
 const {
-  getAllProductsController
+  getAllProductsController,
+  getProductById
 } = require('../controllers/product.controllers')
 const productRoute = express.Router()
 
 productRoute.get('/', wrapAsync(getAllProductsController))
+productRoute.get('/:productId', wrapAsync(getProductById))
 
 module.exports = productRoute
