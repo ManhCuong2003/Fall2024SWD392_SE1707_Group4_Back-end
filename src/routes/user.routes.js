5fc292bd49cd9e78fe1450e1ba83dac307c9592f
ManhCuong2003 <tranmanhcuong11302@gmail.com>
2024-10-10 12:55:18 +0700

    get user infor function

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const userRoute = express.Router()
+const { accessTokenValidator } = require('../middlewares/auth.middlewares')
+const wrapAsync = require('../utils/handler')
+const { getCurrentUserController } = require('../controllers/user.controllers')
+
+userRoute.get(
+  '/get-current-user',
+  accessTokenValidator,
+  wrapAsync(getCurrentUserController)
+)
 
 module.exports = userRoute
